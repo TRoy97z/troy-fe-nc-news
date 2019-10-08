3fbd453057dd525799d0aeaef1bb0b0d72060388
Troy Teelucksingh <troy@Troys-MacBook-Pro-2.local>
2019-10-08 11:29:06 +0100

    created app, made header and nav

---
+++ troy-nc-news/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Components/Header";
+import Nav from "./Components/Nav";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="App">
+      <Header />
+      <Nav />
+    </main>
+  );
+}
+
+export default App;
---
+++ troy-nc-news/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+        Extra! Extra! <br></br>Welcome to NC News
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
